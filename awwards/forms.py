3c3d8eb4515df66ce0720b42490ac43b740883f0
Wanjiru393 <carolciru393@gmail.com>
2022-06-11 13:39:45 +0300

    Created Login View

--- awwards/forms.py
+++ awwards/forms.py
@@ -2,7 +2,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Image, Profile
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=50, label='username', widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
