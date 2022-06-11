9b63e479f4f8e1bec658d99cdde29ae9e27b9420
Wanjiru393 <carolciru393@gmail.com>
2022-06-11 13:46:12 +0300

    Added register page

--- awwards/urls.py
+++ awwards/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path(),
-]
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
 
+]
