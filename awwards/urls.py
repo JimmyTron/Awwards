eb4dd7b11922af0cb6486ef64166bfd29f082a88
Wanjiru393 <carolciru393@gmail.com>
2022-06-13 19:57:57 +0300

    Created new ulrs

--- awwards/urls.py
+++ awwards/urls.py
@@ -9,25 +9,25 @@
 #     path('profile/', views.profile, name='profile')
 # ]
 
-from django.conf.urls import url,include
+from django.urls import include, path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.index,name='Index'),
-    url(r'^create/profile$',views.create_profile, name='create-profile'),
-    url(r'^new/project$',views.new_project, name='new-project'),
-    url(r'^directory/',views.directory, name='directory'),
-    url(r'^profile/',views.profile, name='profile'),
-    url(r'^site/(\d+)',views.site,name='site'),
-    url(r'^search/',views.search_results, name='search_results'),
-    url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    path('',views.index,name='Index'),
+    path('create/profile',views.create_profile, name='create-profile'),
+    path('new/project',views.new_project, name='new-project'),
+    path('directory/',views.directory, name='directory'),
+    path('profile/',views.profile, name='profile'),
+    path('site/(\d+)',views.site,name='site'),
+    path('search/',views.search_results, name='search_results'),
+    path('user/',views.user_profile,name='user-profile'),
 
-    url(r'^api/profiles/$', views.ProfileList.as_view()),
-    url(r'^api/projects/$', views.ProjectList.as_view()),
-    url(r'^api/categories/$', views.categoriesList.as_view()),
-    url(r'^api/technologies/$', views.technologiesList.as_view()),
+    path('api/profiles/', views.ProfileList.as_view()),
+    path('api/projects/', views.ProjectList.as_view()),
+    path('api/categories/', views.categoriesList.as_view()),
+    path('api/technologies/', views.technologiesList.as_view()),
 ]
 
 if settings.DEBUG:
