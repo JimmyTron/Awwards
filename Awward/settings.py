cdc3d370bb5e2729f0b48cdd6e455036ec67f95f
Wanjiru393 <carolciru393@gmail.com>
2023-06-23 12:09:01 +0300

    Bug fix

--- Awward/settings.py
+++ Awward/settings.py
@@ -92,8 +92,6 @@ DATABASES = {
 }
 
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
